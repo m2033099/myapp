5b01b3d83ee7200f1ccd4d259a0497a70e2937c9
m2033099 <m1610600@gmail.com>
2020-12-17 02:31:29 +0900

    for heroku

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
