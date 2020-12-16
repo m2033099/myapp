b7a6d11af55f8a2b27e1b204b28321d481b2d87a
m2033099 <m1610600@gmail.com>
2020-12-17 01:45:44 +0900

    for heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '7k%(nx(82$vr7+fx+_-!tqld7!++ye+yfr+7dlyt2t26f!*0=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
