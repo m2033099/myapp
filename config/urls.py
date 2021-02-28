0284cedae758c8a499f39537cbdb0f4043fbb7a0
m2033099 <m1610600@gmail.com>
2021-02-28 14:39:56 +0900

    change views

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +27,4 @@ urlpatterns = [
     # urlが何もない時に、Home画面に移動させる。
     path('', RedirectView.as_view(url='book/')),
     path('book/', include('book.urls')),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
