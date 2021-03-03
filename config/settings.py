33eb05c0c66a63d3e3e06bc20ccf43d4c0edf439
m2033099 <m1610600@gmail.com>
2021-03-03 11:16:39 +0900

    change templates

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
