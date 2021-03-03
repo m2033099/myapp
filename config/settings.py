17b016fb7df78056d2c2dbf264c881a405ee3548
m2033099 <m1610600@gmail.com>
2021-03-03 11:58:18 +0900

    change templates

--- config/settings.py
+++ config/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Activate Django-Heroku.
