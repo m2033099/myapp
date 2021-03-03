060bc05b84a96eb6d480198beb49e4d8b917132f
m2033099 <m1610600@gmail.com>
2021-03-03 11:39:49 +0900

    change templates

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URLSTATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URLSTATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
 STATIC_URL = '/static/'
 
